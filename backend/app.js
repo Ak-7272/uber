4aa74a319e1e589408c0359e44db82d88fbc7a05
Ak-7272 <akshat2981@gmail.com>
2025-01-30 02:21:47 +0530

    user complete

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,18 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const connectToDb= require('./db/db');
+const userRoutes = require('./routes/user.routes');
 
 connectToDb();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use(cors()); // abhi k lie sara jagah se req accept krnege ,baad m change krenge isko
 app.get('/',(req,res)=>{
     res.send("hello world") // just for checking the server
 })
+
+
+app.use('/users',userRoutes); 
 
 module.exports= app;
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,7 +2,17 @@ const express = require('express');
 const router = express.router();
 
 const {body}=require ('express-validator')
+const userController = require('../controllers/user.controller')
 
+router.post('/register',[
+    body('fullname.firstname').notEmpty().withMessage('First Name is required'),
+   
+    body('email').isEmail().withMessage('Please enter a valid email'),
+    body('password').isLength({min:6}).withMessage('Password must be at least 6 characters long'),
+    body('password').matches(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*/, 'Your password must contain at least one number, one lowercase and one uppercase letter').withMessage('Password must contain at least one number, one lowercase and one uppercase letter'),
+
+    userController.registerUser // it is passed as a user controller 
+])
 
 
 
