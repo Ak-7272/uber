826a57485589f0cef209653af08ec98fdea69d2e
Ak-7272 <akshat2981@gmail.com>
2025-03-22 03:46:04 +0530

    user home done

--- frontend/project/src/components/LocationSearchPanel.jsx
+++ frontend/project/src/components/LocationSearchPanel.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
 
-const LocationSearchPanel = () => {
+const LocationSearchPanel = (props) => {
+
+  const locations=[
+    "24 B, near Police station, Ranchi",
+    "2 C, near Cafe, Ranchi",
+    "21 B, near railway station, Ranchi"
+  ]
+
+
   return (
     <div>
-        <div className='flex gap-4 items-center justify-start my-4'>
+        {
+          locations.map(function(elem,idx){
+            return  <div key={idx} onClick={()=>{
+              props.setvehiclePanel(true)
+              props.setpanelOpen(false)
+            }} className='flex gap-4 border-2 p-3 border-gray-100 active:border-black rounded-xl  items-center justify-start my-2'>
             <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
         
-            <h4 className='font-medium'>23B ,abc college ,k7 layout</h4>
-        </div>
-        <div className='flex gap-4 items-center justify-start my-4'>
-            <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
-        
-            <h4 className='font-medium'>23B ,abc college ,k7 layout</h4>
-        </div>
+            <h4 className='font-medium'>{elem}</h4>
+          </div>
+          })
+        }
+
+       
     </div>
   )
 }
